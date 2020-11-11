347bc7c5f33631a2247aa5c87aa2e28d8b828e0f
Juan Villarin <villjua96@gmail.com>
2020-11-11 10:16:05 -0600

    generated ProductTag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_161057) do
+ActiveRecord::Schema.define(version: 2020_11_11_161511) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2020_11_11_161057) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "product_tags", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_tags_on_category_id"
+    t.index ["product_id"], name: "index_product_tags_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -70,5 +79,7 @@ ActiveRecord::Schema.define(version: 2020_11_11_161057) do
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
   add_foreign_key "orders", "users"
+  add_foreign_key "product_tags", "categories"
+  add_foreign_key "product_tags", "products"
   add_foreign_key "users", "provinces"
 end
