ba4760a9b513121f961a0b914e156d3392c04379
Juan Villarin <villjua96@gmail.com>
2020-11-11 16:26:21 -0600

    added categorical search functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,25 @@
 class ProductsController < ApplicationController
   def index
-    if params["query"].nil? || params["query"] == ''
-      @products = Product.order('name ASC')
+    # Default
+    if params['query'].nil? && params['category'].nil?
+      @products = Product.all
       @header = "All Products"
+    # Redirect default
+    elsif params['query'] == '' && params['category'] == ''
+      redirect_to products_path
+    # Redirect to Category
+    elsif params['query'] == '' && params['category'] != ''
+      redirect_to Category.find(params['category'])
+    # All product search
+    elsif params['query'] != '' && params['category'] == ''
+      @products = Product.where("name LIKE '%#{params['query']}%' OR description LIKE '%#{params['query']}%'")
+      @header = "#{@products.count} #{@products.count == 1 ? "result" : "results"} for '#{params['query']}'"
+    # Categorical search
     else
       @products = Product.left_outer_joins(:categories)
-                         .where("products.name LIKE '%#{params['query']}%' OR categories.name LIKE '%#{params['query']}%'")
-      @header = "#{@products.count} results found for '#{params['query']}'"
+                         .where("categories.id =#{params['category']}")
+                         .where("products.name LIKE '%#{params['query']}%' OR products.description LIKE '%#{params['query']}%'")
+      @header = "#{@products.count} results for '#{params['query']}' in '#{Category.find(params['category']).name}'"
     end
   end
 
